c0d71a9024a59066d438c241d6ae5c700f643143
Jeba-anika <anikanur98@gmail.com>
2022-10-25 19:45:25 +0600

    header component created

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Courses from "../Pages/Courses/Courses";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 
 export const routes = createBrowserRouter([
     {
@@ -10,6 +12,14 @@ export const routes = createBrowserRouter([
             {
               path: '/',
               element: <Home></Home>  
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/courses',
+                element: <Courses></Courses>
             }
         ]
     }
